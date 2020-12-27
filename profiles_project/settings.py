0ec3802ee5266f32f2cdd063e52083686e23b793
Cheng Chen <cheng.chen.2017@marshall.usc.edu>
2020-12-26 23:24:56 -0800

    Added allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'ktt&vjh$bdbfnmz#c%s*d7-!-t)ml%fv93kjqczhvwrj#x@fdd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-52-10-112-194.us-west-2.compute.amazonaws.com', 
+    '127.0.0.1'
+]
 
 
 # Application definition
